352f071fecd50bbeb4e0cd0a63312c56d6554d90
nandy_x <nlegorretar@gmail.com>
2019-11-26 21:32:40 -0600

    chore: remove comments on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,12 @@
 import 'package:architectures/bloc/bloc/movie_bloc.dart';
 import 'package:flutter/material.dart';
 
-// import 'first_version/home_page.dart' as firstVersion;
-// import 'vanilla/vanilla.dart' as vanilla;
-// import 'scopedmodel/scope_model.dart' as scopeModel;
 import 'bloc/bloc/bloc_provider.dart';
 import 'bloc/bloc.dart' as bloc;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // final Widget _page = firstVersion.HomePage();
-  // final Widget _page = vanilla.HomePage(
-  //   repository: vanilla.MovieRepository(),
-  // );
-  // final Widget _page = scopeModel.HomePage(
-  //   provider: scopeModel.MovieProvider(),
-  // );
   final Widget _page = bloc.HomePage();
 
   @override
